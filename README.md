02dd2e14f090e1a0a8de99b7778d2d97772a44cc
natelg1998 <ntlg414@gmail.com>
2022-01-30 21:55:02 -0500

    Adding miceforest

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Python project for DAAN862
 Data Sources:
 https://www.fueleconomy.gov/feg/download.shtml
 
+For MICE explanation
+https://towardsdatascience.com/multiple-imputation-with-random-forests-in-python-dec83c0ac55b
+https://pypi.org/project/miceforest/
+
 From your venv, run this to set up requirements
 pip install -r requirements.txt
 
--- src/epa/epa.py
+++ src/epa/epa.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import miceforest as mf
 
 #Load our data. Update your file path as necessary
 epadata = pd.read_csv("C:\\Users\\ntlg4\\PycharmProjects\\DAAN862Project\\data\\vehicles.csv")
@@ -15,3 +16,36 @@ print(epadata.head())
 print(epadata.describe())
 print(epadata.info())
 print(epadata.isna().sum())
+
+
+
+def calc_missing_values(series):
+    """
+    Calculate percentage of NAs in dataframe
+    :param series:
+    :return:
+    """
+    num = series.isnull().sum()
+    length = len(series)
+    print(round(num/length, 2))
+
+calc_missing_values(epadata)
+
+#Drop any columns with 50% or more values
+epadata = epadata.drop(["guzzler", "trans_dscr", "tCharger", "sCharger", "atvType", "fuelType2", "rangeA",
+                        "evMotor", "mfrCode", "c240Dscr","charge240b", "c240bDscr", "startStop"], axis = 1)
+
+print(epadata.info())
+print(epadata.isna().sum())
+
+#Let's handle the rest of the missing values
+kds = mf.ImputationKernel(
+    data = epadata,
+    save_all_iterations=True,
+    random_state=5345
+)
+
+kds.mice(5)
+
+completed_data = kds.complete_data()
+print(completed_data)
